ea77bd109abaec2cc5da51dd85bf277d1ab22c91
Maksym Malyi <malyimaksym37@gmail.com>
2025-05-12 11:50:23 +0300

    paralell routing only one component at a time

--- nextjs_advaced_course/src/app/about/@someComponentPage/renderOtherComponent/page.tsx
+++ nextjs_advaced_course/src/app/about/@someComponentPage/renderOtherComponent/page.tsx
@@ -2,8 +2,8 @@ import Link from "next/link";
 export default function SomeComponentPage() {
   return (
     <>
-      <h3> Some component </h3>
-      <Link href={"/about/renderOtherComponent"}>Update it</Link>
+      <h3> Some component updated!!</h3>
+      <Link href={"/about"}>Back</Link>
     </>
   );
 }
